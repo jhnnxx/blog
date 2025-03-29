8c8af66e76467790bb3e86667febde527fa4356f
jhnnx <jhnnx.lee@gmail.com>
2025-03-30 00:59:52 +0900

    feat: metadata 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { PostBody } from '@/components/post/post-body'
 import { PostHeader } from '@/components/post/post-header'
 import { getPostBySlug } from '@/lib/api'
 import markdownToHtml from '@/lib/markdownToHtml'
+import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 type Params = {
@@ -38,4 +39,23 @@ export default async function PostPage(props: Params) {
       </Container>
     </main>
   )
+}
+
+export async function generateMetadata(props: Params): Promise<Metadata> {
+  const params = await props.params
+  const post = getPostBySlug(params.slug)
+
+  if (!post) {
+    return notFound()
+  }
+
+  const title = `${post.title} | leeseongjun's Blog 🌱`
+
+  return {
+    title,
+    openGraph: {
+      title,
+      images: [post.ogImage.url],
+    },
+  }
 }
